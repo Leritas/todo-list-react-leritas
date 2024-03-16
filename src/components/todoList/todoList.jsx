41b3930ce8b66149bff707eb9173a75268bca441
leritas <din.fuso@mail.ru>
2024-03-17 00:34:06 +0300

    date added to list items

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -1,21 +1,71 @@
 import { useState } from "react";
 
 export default function TodoList() {
-  const [inputValue, setInputValue] = useState("");
-  const [listOfTodos, setListOfTodos] = useState([]);
+  const [mainInputValue, setMainInputValue] = useState("");
+  const [listOfTodos, setListOfTodos] = useState(
+    JSON.parse(localStorage.getItem("localListOfTodos")) || ""
+  );
 
-  function handleInputChange(e) {
-    setInputValue(e.target.value);
+  const monthList = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+  ];
+
+  function handleMainInputChange(e) {
+    setMainInputValue(e.target.value);
   }
 
-  function handleKeyDown(e) {
+  function handleKeyDownEnter(e) {
     if (e.key === "Enter") {
-      setListOfTodos([...listOfTodos, e.target.value]);
-      setInputValue("");
+      let thisDate = new Date();
+      let wasAdded =
+        "Добавлено " +
+        thisDate.getDate() +
+        " " +
+        monthList[thisDate.getMonth()] +
+        " " +
+        thisDate.getFullYear() +
+        " в " +
+        thisDate.getHours() +
+        ":" +
+        thisDate.getMinutes();
+
+      let updatedList = [
+        ...listOfTodos,
+        { date: wasAdded, completed: false, text: e.target.value },
+      ];
+
+      setListOfTodos(updatedList);
+      localStorage.setItem("localListOfTodos", JSON.stringify(updatedList));
+      setMainInputValue("");
     }
   }
 
-  const listItems = listOfTodos.map((todo) => <li>{todo}</li>);
+  function handleDeleteClick(e) {}
+
+  function handleChangeClick(e) {}
+
+  let listItems;
+
+  if (listOfTodos != "") {
+    listItems = listOfTodos.map((todo, index) => (
+      <li key={index}>
+        <h3>{todo.text}</h3>
+        <span>{todo.date}</span>
+        <button onClick={handleDeleteClick}>delete</button>
+        <button onClick={handleChangeClick}>change</button>
+      </li>
+    ));
+  }
 
   return (
     <div className="todo-container">
@@ -28,9 +78,9 @@ export default function TodoList() {
           type="text"
           name="todo"
           id="add-todo"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
+          value={mainInputValue}
+          onChange={handleMainInputChange}
+          onKeyDown={handleKeyDownEnter}
         />
       </div>
 
